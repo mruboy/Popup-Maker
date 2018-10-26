36bf0d83a8b3514f28ee93306060aabeb6861618
Ryan Yoo <ryoo@dmtgroup.dmt.ca>
2018-10-26 11:28:25 -0500

    Moved function popmake_post from admin-functions.php to general-functions.php to fix issues where cron-based updates on popup posts were failing due to popmake_post not being defined for non admin users

--- includes/general-functions.php
+++ includes/general-functions.php
@@ -15,6 +15,21 @@ if ( ! function_exists( 'maybe_json_attr' ) ) {
 }
 
 /**
+ * Returns $_POST key.
+ *
+ * @since 1.0
+ *
+ * @param string $name is the key you are looking for. Can use dot notation for arrays such as my_meta.field1 which will resolve to $_POST['my_meta']['field1'].
+ *
+ * @return mixed results of lookup
+ */
+function popmake_post( $name, $do_stripslashes = true ) {
+    $value = popmake_resolve( $_POST, $name, false );
+
+    return $do_stripslashes ? stripslashes_deep( $value ) : $value;
+}
+
+/**
  * Returns array key from dot notated array key..
  *
  * @since 1.0
